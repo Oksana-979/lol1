5c2076bb1828179bbf6d05787c21c1bf01d28a67
Oksana-979 <gabenovaoksana@gmail.com>
2025-09-25 09:50:23 +0300

    Initial commit

---
+++ w1.py
@@ -0,0 +1,8 @@
+a = int(input('Введите число a: '))
+b = int(input('Введите число b: '))
+c = int(input('Введите число c: '))
+# Проверяем Условие a * b == c
+if a * b == c:
+    print('УРА!')
+else:
+    print('ФУ!')
